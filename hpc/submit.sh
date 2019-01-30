5c7a174042aa7696c87bd408f064eaa8d193e9f6
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-01-30 18:46:24 +0100

    refactor: notify on aborted job only

--- hpc/submit.sh
+++ hpc/submit.sh
@@ -9,9 +9,9 @@
 # -- specify queue --
 #PBS -q hpc
 # -- mail notification --
-#PBS -m e
+#PBS -m a
 # -- Job array specification --
-#PBS -t 1-20
+#PBS -t 1-20%120
 # Number of nodes and cores per node.
 #PBS -l nodes=1:ppn=2
 # Specify the wall clock time per job in the array.
@@ -24,4 +24,4 @@ cd $PBS_O_WORKDIR
 source ~/miniconda3/bin/activate meta
 
 # Start a job with the desired number of processes (corresponds to `ppn`).
-python3.6 /work3/morbeb/Projects/run.py --processes=2 models.tsv $PBS_ARRAYID
+python3.6 /work3/morbeb/Projects/run.py --processes=1 models.tsv $PBS_ARRAYID
