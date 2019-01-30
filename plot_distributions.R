994ab99011f11eea363c8ced3887ef1f440786b1
Moritz E. Beber <morbeb@biosustain.dtu.dk>
2019-01-30 23:02:35 +0100

    feat: add violin plots

--- plot_distributions.R
+++ plot_distributions.R
@@ -20,12 +20,15 @@ bigg_df <- readr::read_csv("data/bigg/metrics.csv") %>%
 
 uminho_df <- readr::read_csv("data/uminho/metrics.csv")
 
-mmodel_df <- readr::read_csv("data/mmodel/metrics.csv")
+# Contains mostly duplicates.
+# mmodel_df <- readr::read_csv("data/mmodel/sbml/metrics.csv")
+mmodel3_df <- readr::read_csv("data/mmodel/sbml3/metrics.csv")
 
-total_df <- bind_rows(bigg_df, uminho_df, mmodel_df) %>%
+total_df <- bind_rows(bigg_df, uminho_df, mmodel3_df) %>%
   mutate(
     model = factor(model),
-    collection = factor(collection)
+    collection = factor(collection),
+    metric = 1 - metric
   )
 
 # Transform data ----------------------------------------------------------
@@ -51,3 +54,36 @@ for (i in 1:nrow(meta_df)) {
   print(meta_df$plot[[i]])
   ggsave(file.path("figures", sprintf("%s.pdf", meta_df$test[[i]])))
 }
+
+total_df %>%
+  filter(is.finite(metric)) %>%
+ggplot(
+  .,
+  aes(x = metric, fill = collection, color = collection)
+) +
+  geom_freqpoly(binwidth = 0.02) +
+  coord_cartesian(xlim = c(0, 1)) +
+  scale_fill_brewer(palette = "Set1") +
+  scale_color_brewer(palette = "Set1") +
+  xlab("Metric") +
+  ylab("Frequency") +
+  facet_wrap(~ test, scales = "free_y")
+
+ggsave(file.path("figures", "overview.pdf"), width = 84, height = 118, units = "cm")
+
+total_df %>%
+  filter(is.finite(metric)) %>%
+  ggplot(
+    .,
+    aes(x = collection, y = metric, fill = collection)
+  ) +
+  geom_violin(draw_quantiles = c(0.25, 0.5, 0.75)) +
+  geom_jitter(height = 0, width = 0.3, fill = "black", color = "black") +
+  coord_cartesian(ylim = c(0, 1)) +
+  scale_fill_brewer(palette = "Set1") +
+  # scale_color_brewer(palette = "Set1") +
+  xlab("Collection") +
+  ylab("Metric") +
+  facet_wrap(~ test)
+
+ggsave(file.path("figures", "violins.pdf"), width = 84, height = 118, units = "cm")
