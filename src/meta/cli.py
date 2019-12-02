be8a1d61d35653778a405a827fde1f0757bd358e
Midnighter <midnighter@posteo.net>
2019-12-02 16:07:11 +0100

    feat: add model discovery function

--- src/meta/cli.py
+++ src/meta/cli.py
@@ -19,9 +19,12 @@
 """"""
 
 import logging
+import os
+import os.path as osp
 
 import click
 import click_log
+import pandas as pd
 
 from meta.etl import extract_transform_load
 
@@ -59,3 +62,47 @@ def cli():
 def etl(data, output, file_format):
     """Extract all results and transform them to a tabular format."""
     extract_transform_load(data, output, file_format)
+
+
+@cli.command()
+@click.help_option("--help", "-h")
+@click.option(
+    "--file-format",
+    default=".xml.gz",
+    show_default=True,
+    type=click.Choice([".xml.gz", ".xml", ".json.gz", ".json"]),
+    help="Choose the desired file format to look for."
+)
+@click.option(
+    "--filename",
+    default="models.tsv",
+    show_default=True,
+    type=click.Path(exists=False, file_okay=True, dir_okay=False,
+                    writable=True),
+    help="Where to write the table of collected models."
+)
+@click.argument(
+    "paths",
+    metavar="<MODEL PATH> [...]",
+    nargs=-1,
+    type=click.Path(exists=True, file_okay=False),
+)
+def model_table(paths, file_format, filename):
+    """
+    Write models and their paths to a table.
+
+    """
+    models = []
+    results = []
+    for base in paths:
+        for dirpath, dirnames, filenames in os.walk(base):
+            logger.debug(dirpath)
+            for name in filenames:
+                if name.endswith(file_format):
+                    models.append(name[:-len(file_format)])
+                    results.append(osp.join(dirpath, name))
+    df = pd.DataFrame({
+        "model": models,
+        "path": results
+    })
+    df.to_csv(filename, sep="\t", index=False)
